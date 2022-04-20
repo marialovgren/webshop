afe7994bc860b441a24a0fbaf3c718b7b56a832b
marialovgren <marialovgren86@hotmail.com>
2022-04-20 10:40:47 +0200

    created navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 
 // page components
 import Home from './pages/Home'
@@ -12,20 +12,25 @@ function App() {
       <BrowserRouter>
         <nav>
           <h1>Handmade handbags</h1>
+		  <NavLink exact to="/">Home</NavLink>
+		  <NavLink to="/about">About</NavLink>
+		  <NavLink to="/contact">Contact</NavLink>
         </nav>
 
-		<Route path="/">
-			<Home />
-		</Route>
+		<Switch>
+			<Route exact path="/">
+				<Home />
+			</Route>
 
-		<Route path="/about">
-			<About />
-		</Route>
+			<Route path="/about">
+				<About />
+			</Route>
 
-		<Route path="/contact">
-			<Contact />
-		</Route>
-		
+			<Route path="/contact">
+				<Contact />
+			</Route>
+		</Switch>
+
       </BrowserRouter>
     </div>
   );
